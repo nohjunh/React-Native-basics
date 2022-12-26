c234717bcb2d7a91accd5d08592237552ed7dc5f
nohjunh <nsh042777@naver.com>
2022-12-26 16:38:19 +0900

    # Styles part Two

--- NomadWeather/App.js
+++ NomadWeather/App.js
@@ -1,18 +1,40 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Dimensions, ScrollView } from "react-native";
+
+// ScrollView는 기본적으로 세로 스크롤이다.
+// horizontal을 추가하면 가로 스크롤이 된다.
+// contentContainerStyle을 추가하면 스크롤 뷰의 스타일을 지정할 수 있다.
+// ScrollView는 스크린보다 커야하기에 flex size가 의미가 없다.
+// Scorll은 기본 스크린에서 더 커야지 스크롤이 가능하다. (스크롤을 넘겨서 스크롤뷰를 보기 원하기 때문)
+// Dimensions 사용 시 사용자 디바이스 화면 크기를 얻을 수 있다.
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window"); // object안에 있는 width를 가져오고 이걸 SCREEN_WIDTH라 칭함
 
 export default function App() {
   return (
-    <View styles={styles.container}>
+    <View style={styles.container}>
       <View style={styles.city}>
         <Text style={styles.cityName}>Seoul</Text>
       </View>
-      <View style={styles.weather}>
+      <ScrollView
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.description}>Sunny</Text>
         </View>
-      </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -29,21 +51,20 @@ const styles = StyleSheet.create({
   },
   cityName: {
     fontSize: 68,
-    fontWeight: "bold",
+    fontWeight: "500",
   },
-  weather: {
-    flex: 3,
-  },
+  weather: {},
   day: {
+    width: SCREEN_WIDTH,
     flex: 1,
     alignItems: "center",
   },
   temp: {
     marginTop: 50,
-    fontSize: 60,
+    fontSize: 178,
   },
   description: {
-    marginTop: -30,
-    fontsize: 60,
+    marginTop: -20,
+    fontSize: 60,
   },
 });
