0a06a4065e4c4b33b7095eff9194167a18dc8d76
nohjunh <nsh042777@naver.com>
2022-12-26 17:51:16 +0900

    # Location

--- NomadWeather/App.js
+++ NomadWeather/App.js
@@ -1,20 +1,37 @@
-import React from "react";
+import * as Location from "expo-location";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View, Dimensions, ScrollView } from "react-native";
-
-// ScrollView는 기본적으로 세로 스크롤이다.
-// horizontal을 추가하면 가로 스크롤이 된다.
-// contentContainerStyle을 추가하면 스크롤 뷰의 스타일을 지정할 수 있다.
-// ScrollView는 스크린보다 커야하기에 flex size가 의미가 없다.
-// Scorll은 기본 스크린에서 더 커야지 스크롤이 가능하다. (스크롤을 넘겨서 스크롤뷰를 보기 원하기 때문)
-// Dimensions 사용 시 사용자 디바이스 화면 크기를 얻을 수 있다.
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window"); // object안에 있는 width를 가져오고 이걸 SCREEN_WIDTH라 칭함
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+
+  const ask = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+  };
+
+  useEffect(() => {
+    ask();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView
         pagingEnabled
