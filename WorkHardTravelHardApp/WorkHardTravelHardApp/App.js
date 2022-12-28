a38b69971b33ac0343dc7bd7fb2d12df3aa60678
nohjunh <nsh042777@naver.com>
2022-12-28 22:29:47 +0900

    # Touchables, Pressable

--- WorkHardTravelHardApp/WorkHardTravelHardApp/App.js
+++ WorkHardTravelHardApp/WorkHardTravelHardApp/App.js
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Pressable as PressableRaw,
+} from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import { theme } from "./colors";
+
+function Pressable(props) {
+  return (
+    <PressableRaw
+      onPress={props.onPress}
+      hitSlop={props.hitSlop}
+      style={({ pressed }) => [
+        props.style || {},
+        { opacity: pressed ? 0.5 : 1 },
+      ]}
+    >
+      {props.children}
+    </PressableRaw>
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Pressable>
+          <Text style={styles.btnText}>Work</Text>
+        </Pressable>
+        <Pressable>
+          <Text style={styles.btnText}>Travel</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -13,8 +43,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 100,
+  },
+  btnText: {
+    fontSize: 38,
+    fontWeight: "600",
+    color: "white",
   },
 });
